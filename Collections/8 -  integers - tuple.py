93d6971807b475a2a079c8303665c4166314a65f
Kamila Marchlewska <65670940+Kavri@users.noreply.github.com>
2020-05-29 11:52:56 +0200

    collections
    collections

---
+++ Collections/8 -  integers - tuple.py
@@ -0,0 +1,10 @@
+# Make an integer tuple.
+# The user gives any number.
+# If the number is in the program, the program will display the index..
+
+tuple = (1, 2, 3, 4, 5, 6, 7)
+
+number = int(input('Enter the number: '))
+
+if number in tuple:
+    print(f'Digit index: tuple.index(l)')
