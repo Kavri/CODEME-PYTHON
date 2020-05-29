970b7ce92e43df0b6b5ce73944dbc8e49c93d0fe
Kamila Marchlewska <65670940+Kavri@users.noreply.github.com>
2020-05-29 11:52:19 +0200

    collections
    collections

---
+++ Collections/6 - my pet.py
@@ -0,0 +1,10 @@
+# Create a tuple
+# Tuple has your pet's details
+# Unpack the tuples into individual variables
+# Complete the sentence: 'My dog, border collie is called Dyzio'.
+
+tuple = ('cat', 'European breed', 'Pusia')
+
+tuple_list = list(tuple)
+
+print(f'My {tuple_list[0]}, {tuple_list[1]}, is called  {tuple_list[2]}. ')
