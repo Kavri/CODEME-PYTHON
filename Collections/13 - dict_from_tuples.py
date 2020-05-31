ddde0a9259430d6e359e26fb5c09cac97c1da17c
Kamila Marchlewska <65670940+Kavri@users.noreply.github.com>
2020-05-31 23:19:59 +0200

    collections
    collections

---
+++ Collections/13 - dict_from_tuples.py
@@ -0,0 +1,6 @@
+# Create a list or tuple tuples_to_dict that has two-piece tuple.
+# Transform them into a dictionary - dict_from_tuples.
+
+tuples_to_dict = [('Name:', 'Anna'), ('Surname', 'Oz')]
+dict_from_tuples = dict(tuples_to_dict)
+print(dict_from_tuples)
